69540c411cf44a2e027db3f49e6356dfb91f02ba
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-18 07:07:19 -0300

    creating groq_example.py file

---
+++ groq_exaple.py
@@ -0,0 +1,32 @@
+import os
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+# Function to send a question to GPT-4
+def ask_chatgpt(question: str):
+    """"
+    Log function instantiating an groc client.
+    Python function that asks GROC and gets a response.
+    """
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": question,
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+
+    return chat_completion
+
+question = "How much did I sell yesterday?"
+answer   = ask_chatgpt(question)
+
+print(answer)
