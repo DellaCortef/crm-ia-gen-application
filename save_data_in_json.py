c81f92ce913dd5eb99bcf5eacf1394c3bba3137b
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-19 07:01:07 -0300

    adding connect_to_postgre function

--- save_data_in_json.py
+++ save_data_in_json.py
@@ -2,5 +2,20 @@ import os
 import json
 import psycopg2
 from decimal import Decimal
+from dotenv import load_dotenv
 from datetime import datetime, time
+
+# Load enviroment variables
+load_dotenv()
+
+# Function to connect to PostgreSQL database
+def connect_to_postgre():
+    conn = psycopg2.connect(
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASS")
+    )
+    print(conn)
+    return conn
 
