cc45a601ae086ab9c1ee237bc26419176fabf72c
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-20 07:49:12 -0300

    searching tables in my db, loading and saving into json

--- save_data_in_json.py
+++ save_data_in_json.py
@@ -18,7 +18,7 @@ def connect_to_postgre():
     )
     print(conn)
     return conn
-
+"""
 # Function to load data from gold_sales_by_product
 def load_data_gold_sales_by_product():
     conn = connect_to_postgre()
@@ -42,6 +42,31 @@ def load_data_gold_sales_by_seller():
     data_dict = [dict(zip(columns, row)) for row in data]
     conn.close
     return data_dict
+"""
+
+# Function to load data from gold_aggregation_of_sales_by_seller
+def load_data_gold_aggregation_of_sales_by_seller():
+    conn = connect_to_postgre()
+    query = "SELECT * FROM gold_aggregation_of_sales_by_seller;"
+    with conn.cursor() as cursor:
+        cursor.execute(query)
+        data = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+    data_dict = [dict(zip(columns, row)) for row in data]
+    conn.close
+    return data_dict
+
+# Function to load data from gold_last_seven_days
+def load_data_gold_last_seven_days():
+    conn = connect_to_postgre()
+    query = "SELECT * FROM gold_last_seven_days;"
+    with conn.cursor() as cursor:
+        cursor.execute(query)
+        data = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+    data_dict = [dict(zip(columns, row)) for row in data]
+    conn.close
+    return data_dict
 
 # Custom serializer for non-serializable types
 def custom_serializer(obj):
@@ -58,10 +83,17 @@ def save_in_json(data, file_path):
 
 # Main execution: read data and save into JSON files
 if __name__ == "__main__":
+    '''
     sales_data_by_seller = load_data_gold_sales_by_seller()
     save_in_json(sales_data_by_seller, "gold_sales_by_seller.json")
 
     sales_data_by_product = load_data_gold_sales_by_product()
     save_in_json(sales_data_by_product, "gold_sales_by_product.json")
+    '''
+    sales_data_by_seller = load_data_gold_aggregation_of_sales_by_seller()
+    save_in_json(sales_data_by_seller, "gold_aggregation_of_sales_by_seller.json")
+
+    sales_data_by_product = load_data_gold_last_seven_days()
+    save_in_json(sales_data_by_product, "gold_last_seven_days.json")
 
     print("Data saved in 'gold_vendas_por_vendedor.json' and 'gold_vendas_por_produto.json'.")
